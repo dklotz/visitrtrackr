d857eb8d376a0c27007d361a69969e48b9ff3fe1
David Klotz <david@sofaecke.org>
2013-06-10 19:56:00 +0200

    And now the real empty rails skeleton...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Visitrtrackr::Application.config.secret_key_base = 'd555f6ac89556ecc2cae07b73e705f8e81aae56c7b4f170d251a435eb72d5498f6e8fa69458658240f27afd689688f7dd04dce385f603fe6eb572e9b5e6b5d16'
