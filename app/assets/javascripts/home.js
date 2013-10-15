5d21580cadc9d950f51a95957f697a698d49a214
David Klotz <david@sofaecke.org>
2013-10-16 00:15:47 +0200

    Now showing all visits in map.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -15,11 +15,42 @@ function save_visit(latitude, longitude, user_agent) {
 }
 
 // Updates the page with the geolocation result
-function update_result(latitude, longitude, user_agent) {
-	// TODO: Load all visits: $.getJSON('http://...', function(data) {
-    //            $( "#status").html=data.date; // ...
-    //      });
+function update_visits(latitude, longitude, user_agent) {
 	$( "#the-result" ).html( 'You come from (' + latitude + ',' + longitude + ') and your browser is: ' + user_agent );
+	
+	save_visit(latitude, longitude, user_agent);
+}
+
+function show_visits_results(visits, map) {
+	$( "#status").html("We have " + visits.length + " visits.");
+	
+	var mapBounds = new google.maps.LatLngBounds();
+	
+	$.each(visits, function(index, visit) {
+		var visitLocation = new google.maps.LatLng(visit.latitude, visit.longitude);
+		
+		mapBounds.extend(visitLocation);
+		
+		var marker = new google.maps.Marker({
+			position: visitLocation,
+			map: map,
+			title: visit.useragent
+		});
+	});
+	
+	map.setCenter(mapBounds.getCenter());
+	map.fitBounds(mapBounds);
+}
+
+function show_visits(map) {
+	$( "#status").html("I can haz visits?");
+	// Load all visits: 
+	$.getJSON('/visits.json', function(result) {
+		show_visits_results(result, map);
+	});
+}
+
+function show_map(latitude, longitude) {
 	
 	$( "#status").html("I can haz Maps?");
 	
@@ -35,11 +66,11 @@ function update_result(latitude, longitude, user_agent) {
 	
 	var map = new google.maps.Map(document.getElementById("geo-wrapper"), mapOptions);
 	
-	var marker = new google.maps.Marker({
-		position: myLoc,
-		map: map,
-		title:"Hello World!"
-	});
+	//var marker = new google.maps.Marker({
+	//	position: myLoc,
+	//	map: map,
+	//	title:"Hello World!"
+	//});
 	
-	save_visit(latitude, longitude, user_agent);
+	return map;
 }
